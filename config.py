5b215c49c806b81aed84fa878b0fffbda28176eb
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-09-19 12:38:42 +0300

    Updated the configuration file

---
+++ config.py
@@ -0,0 +1,39 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    """
+    Parent class for the configuration of the app
+    """
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+class DevConfig(Config):
+    """
+    development child class
+    """
+    DEBUG = True
+
+class ProdConfig(Config):
+    """
+    production sub class
+    """
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+
+config_options = {
+    'development' : DevConfig,
+    'production' : ProdConfig
+}
