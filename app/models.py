e083b7988d60f20bc066ba48452698d8c9c74522
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-09-19 20:19:24 +0300

    Updated the comment, upvote and downvote classes

--- app/models.py
+++ app/models.py
@@ -58,10 +58,9 @@ class UserPitch(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     time = db.Column(db.DateTime, default=datetime.utcnow())
     category = db.Column(db.String(255), index=True, nullable=False)
-
-    # comment = db.relationship('Comment',backref='pitch',lazy='dynamic')
-    # upvote = db.relationship('Upvote',backref='pitch',lazy='dynamic')
-    # downvote = db.relationship('Downvote',backref='pitch',lazy='dynamic')
+    comment = db.relationship('Comment', backref='pitch', lazy='dynamic')
+    upvote = db.relationship('Upvote', backref='pitch', lazy='dynamic')
+    downvotes = db.relationship('Downvotes', backref='pitch', lazy='dynamic')
 
     def save_pitch(self):
         db.session.add(self)
@@ -69,3 +68,63 @@ class UserPitch(db.Model):
 
     def __repr__(self):
         return f'Pitch {self.post}'
+
+
+@dataclass
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.Text(), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'), nullable=False)
+
+    def save_c(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls, pitch_id):
+        return Comment.query.filter_by(pitch_id=pitch_id).all()
+
+    def __repr__(self):
+        return f'comment:{self.comment}'
+
+
+@dataclass
+class Upvote(db.Model):
+    __tablename__ = 'upvotes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_upvotes(cls, id):
+        return Upvote.query.filter_by(pitch_id=id).all()
+
+    def __repr__(self):
+        return f'{self.user_id}:{self.pitch_id}'
+
+
+@dataclass
+class Downvotes(db.Model):
+    __tablename__ = 'downvotes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_downvotes(cls, id):
+        return Downvotes.query.filter_by(pitch_id=id).all()
+
+    def __repr__(self):
+        return f'{self.user_id}:{self.pitch_id}'
