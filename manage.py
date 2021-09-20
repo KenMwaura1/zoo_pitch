49f090ba238f981b98aa305028c50de535b07852
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-09-20 06:00:38 +0300

    Heroku deployments

--- manage.py
+++ manage.py
@@ -27,3 +27,6 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+if __name__ == '__main__':
+    app.run()
